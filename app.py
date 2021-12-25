849b87f29efdc8f4acfb912b586506cbc914bcc6
Rhomboic <lynxdroid2@gmail.com>
2021-12-24 20:22:45 -0500

    created game files and dependencies

---
+++ app.py
@@ -0,0 +1,18 @@
+from consts import *
+from game2d import *
+from wave import *
+from models import *
+import random
+import pprint
+
+
+class PySnake(GameApp):
+
+    def start(self):
+        pass
+
+    def update(self, dt):
+        pass
+
+    def draw(self):
+        pass
---
+++ models.py
@@ -0,0 +1,8 @@
+
+from consts import *
+from game2d import *
+from consts import *
+
+
+class Segment(GRectangle):
+    pass
